e2289e24b4a4af85a3230a3a02841dbf4be90f9a
arsikz3 <arsen.ziyadinov@gmail.com>
2022-08-19 09:51:18 +0300

    коммент  задания 2.1

--- bin/dart_home_work.dart
+++ bin/dart_home_work.dart
@@ -3,7 +3,7 @@ import 'package:dart_home_work/model.dart';
 //mport 'class.dart';
 
 void main() {
-  final _workOne = WorkOne(24, 42);
+  //final _workOne = WorkOne(24, 42);
   //print('Задание 2.1');
   //print(_workOne.nodFunct().toString());
   //print(_workOne.nokFunct().toString());
